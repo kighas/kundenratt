d9be4511aa13835ecd2b94a428736c38b803eaca
kighas <elias.ronnmark@elev.ga.ntig.se>
2023-12-19 09:26:24 +0100

    Community tab continuation + chat box

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-// Denna fil skall innehålla JavaScript (JS)
+function sendMessage() {
+    const messageInput = document.getElementById('message_input');
+    const chatMessage = document.getElementById('chat_messages');
+}
+
+if (messageInput.ariaValueMax.trim() !== '') {
+    const messageDiv = document.createElement('div');
+    messageDiv.className = 'message' ;
+    messageDiv.textContent = messageInput.value;
+    chatMessages.appendChild(messageDiv);
+    messageInput.value = '';
+}
